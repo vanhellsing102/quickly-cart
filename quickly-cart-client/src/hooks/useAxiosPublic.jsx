277da728a86363cd578217f932f9ae532191ef44
murad <vvanhellsing30@gmail.com>
2025-04-29 10:37:17 +0600

    or

--- quickly-cart-client/src/hooks/useAxiosPublic.jsx
+++ quickly-cart-client/src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://quickly-cart-server.vercel.app",
     withCredentials: true,
 })
 const useAxiosPublic = () => {
